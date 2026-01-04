0c85f0b008d333c3e65702b8e95a7e22132a5ee2
Syed Omar <syed.omar@ucalgary.ca>
2026-01-04 00:50:19 -0700

    adding two new python questions

--- python_questions.py
+++ python_questions.py
@@ -26,6 +26,57 @@ def python_question_1(tries):
             else:
                 print("You only have " + str(tries - i - 1) +" tries left.")
 
+def python_question_2(tries):
+    global correct_answers 
+    print("Programming Language: Python")
+    print("Write a comment.")
+
+    for i in range(tries):
+        answer = input()
+        if answer.find("#", 0, 1) != -1:
+            print("Success!")
+            correct_answers += 1
+            break
+        else:
+            if (tries - i) == 1:
+                print("Failure!")
+                print("Do you want to know the correct answer? Input yes or no.")
+                truth = input()
+                if truth == "Yes" or truth == "yes":
+                    print("Examples of correct answers:")
+                    print('#test')
+                    print('#hello there')
+                    print('#my name is Bob')
+            elif (tries - i) == 2:
+                print("You only have 1 try left.")
+            else:
+                print("You only have " + str(tries - i - 1) +" tries left.")    
+
+def python_question_3(tries):
+    global correct_answers 
+    print("Programming Language: Python")
+    print("Write a variable x with the value of 11.")
+
+    for i in range(tries):
+        answer = input()
+        if answer.replace(" ", "") == 'x=11':
+            print("Success!")
+            correct_answers += 1
+            break
+        else:
+            if (tries - i) == 1:
+                print("Failure!")
+                print("Do you want to know the correct answer? Input yes or no.")
+                truth = input()
+                if truth == "Yes" or truth == "yes":
+                    print("Correct Answer:")
+                    print('x = 11')
+            elif (tries - i) == 2:
+                print("You only have 1 try left.")
+            else:
+                print("You only have " + str(tries - i - 1) +" tries left.")    
+                          
+
 def total_correct():
     return correct_answers
 
