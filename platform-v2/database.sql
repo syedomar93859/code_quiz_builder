c90aec5f4a92b0c74035b87f95433f74a5989f12
Syed Omar <syed.omar@ucalgary.ca>
2026-03-10 04:00:33 -0600

    cleaning the contents of the SQL file

---
+++ platform-v2/database.sql
@@ -0,0 +1,23 @@
+-- CREATE TABLE QUESTIONS (
+--     id TEXT NOT NULL PRIMARY KEY,
+--     language TEXT NOT NULL,
+--     type TEXT NOT NULL,
+--     difficulty TEXT NOT NULL,
+--     prompt TEXT NOT NULL,
+--     options TEXT NOT NULL,
+--     solution TEXT NOT NULL,
+--     multiple_answers BOOLEAN NOT NULL
+-- );
+
+-- INSERT INTO questions VALUES (
+--     'Q1',
+--     'java',
+--     'multiple_choice',
+--     'easy',
+--     'What is 2 + 2?',
+--     '1,2,3,4',
+--     '4',
+--     0
+-- );
+
+SELECT * FROM questions;
