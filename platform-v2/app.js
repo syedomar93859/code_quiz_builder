2990791a1ab72f46832da976a0c87ee72dd67d25
Syed Omar <syed.omar@ucalgary.ca>
2026-03-16 01:10:30 -0600

    setting up node javascript file for the question builder website page

--- platform-v2/app.js
+++ platform-v2/app.js
@@ -1,44 +1,44 @@
-const sqlite3 = require('sqlite3').verbose();
-let sql;
+// const sqlite3 = require('sqlite3').verbose();
+// let sql;
 
-// connect to DB
-const db = new sqlite3.Database('./questions.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.error(err.message);
-});
-
-// Create table
-// sql = 'CREATE TABLE QUESTIONS (id TEXT NOT NULL PRIMARY KEY, language TEXT NOT NULL, type TEXT NOT NULL, difficulty TEXT NOT NULL, prompt TEXT NOT NULL, options TEXT NOT NULL, solution TEXT NOT NULL, multiple_answers BOOLEAN NOT NULL)';
-// db.run(sql);
-
-// Drop table
-// db.run("DROP TABLE questions");
-
-// Insert data into table
-// sql = 'INSERT INTO questions(id, language, type, difficulty, prompt, options, solution, multiple_answers) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-// db.run(
-//     sql,
-//     ["Q1", "java", "multiple_choice", "easy", "What is 2 + 2?", "1,2,3,4", "4", 0],
-//     (err) => {
-//     if (err) return console.error(err.message);
-// })
-
-//update data
-// sql = `UPDATE questions SET language = ? WHERE id = ?`;
-// db.run(sql, ["python", "Q1"], (err) => {
+// // connect to DB
+// const db = new sqlite3.Database('./questions.db', sqlite3.OPEN_READWRITE, (err) => {
 //     if (err) return console.error(err.message);
 // });
 
-//delete data
-sql = `DELETE FROM questions WHERE id = ?`;
-db.run(sql, ["Q1"], (err) => {
-    if (err) return console.error(err.message);
-});
+// // Create table
+// // sql = 'CREATE TABLE QUESTIONS (id TEXT NOT NULL PRIMARY KEY, language TEXT NOT NULL, type TEXT NOT NULL, difficulty TEXT NOT NULL, prompt TEXT NOT NULL, options TEXT NOT NULL, solution TEXT NOT NULL, multiple_answers BOOLEAN NOT NULL)';
+// // db.run(sql);
 
-// query the data
-sql = `SELECT * FROM questions`;
-db.all(sql, [], (err, rows) =>{
-    if (err) return console.error(err.message);
-    rows.forEach((row) =>{
-        console.log(row);
-    });
-});
+// // Drop table
+// // db.run("DROP TABLE questions");
+
+// // Insert data into table
+// // sql = 'INSERT INTO questions(id, language, type, difficulty, prompt, options, solution, multiple_answers) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+// // db.run(
+// //     sql,
+// //     ["Q1", "java", "multiple_choice", "easy", "What is 2 + 2?", "1,2,3,4", "4", 0],
+// //     (err) => {
+// //     if (err) return console.error(err.message);
+// // })
+
+// //update data
+// // sql = `UPDATE questions SET language = ? WHERE id = ?`;
+// // db.run(sql, ["python", "Q1"], (err) => {
+// //     if (err) return console.error(err.message);
+// // });
+
+// //delete data
+// sql = `DELETE FROM questions WHERE id = ?`;
+// db.run(sql, ["Q1"], (err) => {
+//     if (err) return console.error(err.message);
+// });
+
+// // query the data
+// sql = `SELECT * FROM questions`;
+// db.all(sql, [], (err, rows) =>{
+//     if (err) return console.error(err.message);
+//     rows.forEach((row) =>{
+//         console.log(row);
+//     });
+// });
---
+++ platform-v2/question-builder.js
@@ -0,0 +1,17 @@
+// import http package
+const http = require('node:http');
+
+// create a new server
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// the server is created using this method
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
